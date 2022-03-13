e998a53a9c506b3b9065d64f1bf4b3f3663b0f71
Calandra Lark <anthony.abbott@calandralark.com>
2022-03-13 16:23:52 +0000

    Added and now declares document

---
+++ US-States/Controllers/SplitViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplitViewController.swift
+//  US-States
+//
+//  Created by Anthony Abbott on 13/03/2022.
+//
+
+import UIKit
+
+var calDocument = CalDocument(fileName: "AllStates")
+
+class SplitViewController: UISplitViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
